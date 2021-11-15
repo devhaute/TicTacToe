f3b03150974e6e79c44422fa405c48f4c8a8aad5
devhaute <devhaute@gmail.com>
2021-11-16 04:18:52 +0900

    게임 레이아웃 및 탭 동작 구현

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -1,9 +1,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
+    func isSquareOccupied(_ move: Move?) -> Bool {
+        move == nil ? false : true
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { proxy in
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Circle()
+                            .frame(width: proxy.size.width / 3 - 20, height: proxy.size.width / 3 - 20)
+                        
+                        Image(systemName: moves[index]?.indicator ?? "")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .foregroundColor(.white)
+                    }
+                    .onTapGesture {
+                        if isSquareOccupied(moves[index]) { return }
+                            
+                        self.moves[index] = Move(player: self.isHumansTurn ? .human : .computer, boardIndex: index)
+                        self.isHumansTurn.toggle()
+                    }
+                }
+            }
+            .padding(5)
+            .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "circle" : "xmark"
     }
 }
 
