5b2436029164fc5d7bdf896bc5c9d7da38214536
devhaute <devhaute@gmail.com>
2021-11-16 02:49:23 +0900

    초기 환경 세팅

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by devhaute on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
